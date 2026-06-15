ec2941f9af80d8c6af979e71e3b8d4a382a6db76
CAPi0807 <134230529+CAPi0807@users.noreply.github.com>
2026-06-15 11:10:10 +0100

    Export Kata 3

---
+++ Kata 3/src/main/java/software/ulpgc/katas/App.java
@@ -0,0 +1,28 @@
+package software.ulpgc.katas;
+
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        var cargador = new CargadorDatos();
+        List<Empleado> empleados = cargador.cargarDesdeCSV("src/main/resources/datos.csv");
+
+        var generator = new HistogramGenerator();
+
+        Histogram histDepto = generator.compute(empleados, Empleado::departamento);
+
+        System.out.println("---Histograma Departamento---");
+        for (String key : histDepto.keys()) {
+            System.out.println(key + ": " + histDepto.valueOf(key));
+        }
+
+        Histogram histEdad = generator.compute(empleados, e -> e.edad());
+        System.out.println("---Histograma Edades (décadas)---");
+        for (String key : histEdad.keys()) {
+            System.out.println(key + ": " + histEdad.valueOf(key));
+        }
+
+    }
+}
